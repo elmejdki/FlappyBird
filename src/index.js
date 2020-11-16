4eba490567ac8b207a4049133389cdd7e55cb814
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-11-16 23:55:06 +0100

    Connect the scenes together

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import Phaser from 'phaser';
+import LoadScene from './scenes/LoadScene';
+import MenuScene from './scenes/MenuScene';
+import MainScene from './scenes/MainScene';
+import GameOverScene from './scenes/GameOverScene';
+import './style.scss';
 
+const config = {
+  type: Phaser.AUTO,
+  width: 288,
+  height: 512,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: [
+    LoadScene,
+    MenuScene,
+    MainScene,
+    GameOverScene,
+  ],
+};
+
+// eslint-disable-next-line no-unused-vars
+const game = new Phaser.Game(config);
