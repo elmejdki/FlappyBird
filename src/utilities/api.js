eef84476aa147a1f079bfd07523f97c06d3a0a93
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-11-17 16:18:10 +0100

    Add api help methods to fetch and post data

---
+++ src/utilities/api.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/prefer-default-export */
+// real game id: gO64LtUPnA6GnKbngdU2
+// fake game id: 2uxnCb3QMbmkeb6NKsmo
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/2uxnCb3QMbmkeb6NKsmo/scores';
+
+// Submit data to the Api
+export const postScore = async (username, score) => {
+  const data = {
+    user: username,
+    score,
+  };
+
+  const response = await fetch(url, {
+    method: 'POST',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  if (response.status === 200) {
+    return true;
+  }
+
+  return result.message;
+};
+
+// Get results from the Api
+export const getScores = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.result;
+};
